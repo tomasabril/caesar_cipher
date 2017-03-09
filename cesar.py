c73d9e91e7c9b355d086f638f60f9e1cd2ce5b54
tomas <tomasabril@users.noreply.github.com>
2017-03-09 17:26:32 -0300

    Add files via upload

---
+++ cesar.py
@@ -0,0 +1,19 @@
+
+print("--Criptografia Cesar--\n")
+frase_original = input("digite a frase a ser criptografada:\n")
+
+k = int(input("valor de k: "))
+
+print("aaaaaa")
+
+print(frase_original)
+print(k)
+
+#frase_mod = ord(frase_original)
+
+frase_mod[]
+
+for i in range(len(frase_original)):
+    frase_mod[i] = ord(frase_original[i]) + k
+
+print(chr(frase_mod))
