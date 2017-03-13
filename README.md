b0b32bec989e7a7be94dccd5d02b4b0a7d2816eb
tomas <tomas.abril@gmail.com>
2017-03-12 23:11:31 -0300

    executa pela cmd
    deixando como o professor pediu

--- README.md
+++ README.md
@@ -2,3 +2,22 @@
 exercicio seguranca de sistemas
 
 
+O cifrador de César
+
+Atividade: Produzir uma implementação do cifrador de César. Ele deve funcionar da seguinte forma:
+
+Para cifrar:
+
+cesar -c -k 5 < texto-aberto.txt > texto-cifrado.txt
+
+Para decifrar:
+
+cesar -d -k 5 < texto-cifrado.txt > texto-aberto.txt
+
+Opções:
+
+    -c : cifrar
+    -d : decifrar
+    -k n : valor da chave a ser usada
+
+A rotação de caracteres deve ser feita somente sobre os caracteres [A-Z,a-z,0-9]. Caracteres acentuados devem ser tratados sem acento.
--- cesar.py
+++ cesar.py
@@ -1,22 +1,81 @@
+# Tomás Abril
+# --Criptografia Cesar--
 
-print("--Criptografia Cesar--\n")
-frase_original = input("digite a frase a ser criptografada:\n").lower()
-
-k = int(input("valor de k: "))
-
-
-frase_mod = ""
-
-for i in range(len(frase_original)):
-    if 96 < ord(frase_original[i]) < 123 :
-        num = ord(frase_original[i]) - 97 + k
-        num = num%26
-        num = num + 97
-        frase_mod += chr(num)
-    else:
-        frase_mod += frase_original[i]
+import sys
+import getopt
 
 
+# import unidecode
 
-print("\nfrase criptografada:\n" + frase_mod)
+# ---
 
+def criptografa(des, k, in_name, out_name):
+    if des:
+        k *= -1
+
+    f_in = open(in_name, 'r')
+    texto_original = f_in.read()
+    frase_mod = ""
+
+    # pra tirar os acentos
+    #    texto_original = unidecode._unidecode(texto_original)
+
+    for i in range(len(texto_original)):
+        # numeros
+        if 48 <= ord(texto_original[i]) <= 57:
+            num = ord(texto_original[i]) - 48 + k
+            num = num % 10
+            num = num + 48
+            frase_mod += chr(num)
+        # letras minusculas
+        elif 97 <= ord(texto_original[i]) <= 122:
+            num = ord(texto_original[i]) - 97 + k
+            num = num % 26
+            num = num + 97
+            frase_mod += chr(num)
+        # letras MAIUSCULAS
+        elif 65 <= ord(texto_original[i]) <= 90:
+            num = ord(texto_original[i]) - 65 + k
+            num = num % 26
+            num = num + 65
+            frase_mod += chr(num)
+        # oytros caracteres permanecem
+        else:
+            frase_mod += texto_original[i]
+        # salvando no arquivo de saida
+        f_out = open(out_name, 'w')
+        f_out.write(frase_mod)
+        f_in.close()
+        f_out.close()
+
+
+def main(argv):
+    k = 0
+    desc = False
+    try:
+        opts, args = getopt.getopt(argv, "hcdk:i:o:")
+    except getopt.GetoptError:
+        print('erro ao interpretar comando\nmodo de uso:\ncesar.py -c -k 3 -i input.txt -o output.txt')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print("modo de uso:\ncesar.py -c -k 3 -i texto.txt -o saida.txt\nopcoes:\n"
+                  + " -i :arquivo de entrada\n -o :arquivo de saida"
+                  + " -c :criptografa\n -d :descriptografa\n -k n :valor da chave")
+            sys.exit()
+        elif opt in ("-i"):
+            inputfile_name = arg
+        elif opt in ("-o"):
+            outputfile_name = arg
+        elif opt in ("-c"):
+            desc = False
+        elif opt in ("-d"):
+            desc = True
+        elif opt in ("-k"):
+            k = int(arg)
+
+    criptografa(desc, k, inputfile_name, outputfile_name)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
